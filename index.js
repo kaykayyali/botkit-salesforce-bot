d10e02630d8cc4708e3631f3dc06e666f0b7d0aa
Kaysser Kayyali <kaysser.kayyali@lmrc7763.rcoffice.ringcentral.com>
2017-08-23 15:24:20 -0400

    Update

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var Botkit = require('botkit'),
     http = require('http'),
     request = require('request'),
     formatter = require('./modules/glip-formatter'),
-    salesforce = require('./modules/salesforce');
+    salesforce = require('./modules/salesforce'),
+    Bitfinex = require('bitfinex'),
+    bitfinex = new Bitfinex(process.env.BITFINEX_KEY, process.env.BITFINEX_SECRET);
+ 
 
 
 
@@ -36,150 +39,43 @@ controller.setupWebserver(process.env.port || 3000, function(err, webserver){
 
 
 
-controller.hears(['salesforce: search account (.*)', 'salesforce: search (.*) in accounts'], 'message_received', function (bot, message) {
-    var name = message.match[1];
-    salesforce.findAccount(name).then(function (accounts) {
-        console.log(JSON.stringify(accounts));
-        if(accounts && accounts.length > 0){
-            var data = null;
-            data = "I found these accounts matching  '" + name + "':\n\n"
-            accounts.forEach(function (account) {
-                data += "**Name** \n\n"
-                data +=  account.get("Name") + "\n\n"
-                data += "**Link**"
-                data += "https://login.salesforce.com/" + account.getId() + "\n\n"
-                data += "**Phone**"
-                data += account.get("Phone") + "\n\n"
-                data += "**Address**"
-                data += account.get("BillingStreet") + ", " + account.get("BillingCity") + " " + account.get("BillingState") + "\n\n"
-            })
-            bot.reply(message, data);
-        } else {
-            bot.reply(message, "No Accounts found for " + name );
+controller.hears(['Hey', 'hey', 'Hi', 'hi'], 'message_received', function (bot, message) {
+    console.log("Message is", message);
+    bot.reply(message, 'Hello');
+});
+
+controller.hears(['check bitcoin'], 'message_received', function (bot, message) {
+    bitfinex.ticker('btcusd', function(error, data) {
+        if (error) {
+            bot.reply(message, 'Ooops' + error);
+            return;
         }
+        console.log(data);
+        bot.reply(message, 'BTC current price ' + data.last_price);
     });
 });
 
-controller.hears(['salesforce: search contact (.*)', 'salesforce: find contact (.*)'], 'message_received', function (bot, message) {
-    var name = message.match[1];
-    salesforce.findContact(name).then(function (contacts) {
-        console.log(JSON.stringify(contacts));
-
-        if (contacts && contacts.length > 0) {
-            var data = null;
-            data = "I found these contacts matching  '" + name + "':\n\n"
-            contacts.forEach(function (contacts) {
-                data += "**Name**\n\n"
-                data += contacts.get("Name") + "\n\n"
-                data += "**Email**\n\n"
-                data += contacts.get("Email") + "\n\n"
-                data += "**phone**\n\n"
-                data +=  contacts.get("Phone") + "\n\n"
-                data += "**Link**\n\n"
-                data += "https://login.salesforce.com/" + contacts.getId()+ "\n\n"
-            })
-            bot.reply(message, data);
-        } else {
-            bot.reply(message, "No contacts found for " + name );
+controller.hears(['check ethereum'], 'message_received', function (bot, message) {
+    bitfinex.ticker('ethusd', function(error, data) {
+        if (error) {
+            bot.reply(message, 'Ooops' + error);
+            return;
         }
+        console.log(data);
+        bot.reply(message, 'ETH current price ' + data.last_price);
     });
 });
 
-controller.hears(['salesforce: top (.*) deals', 'salesforce: top (.*) opportunities'], 'message_received', function (bot, message) {
-    var count = message.match[1];
-    console.log("count:" + count);
-    salesforce.getTopOpportunities(count).then(function (opportunities) {
-        console.log(opportunities);
-        if (opportunities && opportunities.length > 0) {
-            var data = null;
-            data = "Here are your top " + count + " records :\n\n"
-            opportunities.forEach(function (opportunity) {
-                data += "**Name**\n"
-                data += opportunity.get("Name") + "\n\n"
-                data += "**Stage**\n"
-                data += opportunity.get("StageName")+ "\n\n"
-                data += "**Amount**\n"
-                data += new Intl.NumberFormat('en-US', {
-                        style: 'currency',
-                        currency: 'USD'
-                    }).format(opportunity.get("Amount")) + "\n\n"
-                data += "**Probability**\n"
-                data +=  opportunity.get("Probability") + "% \n\n"
-                data += "**Link**\n"
-                data += "https://login.salesforce.com/" + opportunity.getId() + "\n\n"
-            });
-
-            return bot.reply(message, data);
-        } else {
-            return bot.reply(message, "No new opportunities found");
+controller.hears(['check litecoin'], 'message_received', function (bot, message) {
+    bitfinex.ticker('ltcusd', function(error, data) {
+        if (error) {
+            bot.reply(message, 'Ooops' + error);
+            return;
         }
+        console.log(data);
+        bot.reply(message, 'LTC current price ' + data.last_price);
     });
 });
-
-controller.hears(['salesforce: search opportunity (.*)', 'salesforce: find opportunity (.*)'], 'message_received', function (bot, message) {
-    var name = message.match[1];
-    salesforce.findOpportunity(name).then(function (opportunities) {
-        console.log(JSON.stringify(opportunities));
-        if (opportunities && opportunities.length > 0) {
-            var data = null;
-            data = "I found these opportunities matching  '" + name + "': \n\n"
-            opportunities.forEach(function (opportunity) {
-                data += "**Name**\n\n"
-                data +=  opportunity.get("Name") + "\n\n"
-                data += "**Stage**\n\n"
-                data +=  opportunity.get("StageName") + "\n\n"
-                data += "**Amount**\n\n"
-                data += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(opportunity.get("Amount")) + "\n\n"
-                data += "**Probability**\n\n"
-                data += opportunity.get("Probability") + "% \n\n"
-                data += "**Link**\n\n"
-                data += "https://login.salesforce.com/" + opportunity.getId()+ "\n\n"
-            });
-            bot.reply(message, data);
-        }else {
-            bot.reply(message, "No new opportunites found for " + name );
-        }
-
-    });
-
-});
-
-controller.hears(['salesforce: create case', 'salesforce: new case'], 'message_received', function (bot, message) {
-
-    var subject, description = null;
-
-    var askSubject = function askSubject(response, convo) {
-
-        convo.ask("What's the subject?", function (response, convo) {
-            subject = response.text;
-            askDescription(response, convo);
-            convo.next();
-        });
-    };
-
-    var askDescription = function askDescription(response, convo) {
-
-        convo.ask('Enter a description for the case', function (response, convo) {
-            description = response.text;
-            salesforce.createCase({ subject: subject, description: description }).then(function (_case) {
-                var data = null;
-                data = "New Case Created: \n\n"
-                data += "**Subject**\n\n"
-                data +=  _case.get("subject") + "\n\n"
-                data += "**Description**\n\n"
-                data += _case.get("description") + "\n\n"
-                data += "**Link**\n\n"
-                data += "https://login.salesforce.com/" + _case.get("id") + "\n\n"
-                bot.reply(message, data);
-                convo.next();
-            });
-        });
-    };
-
-    bot.reply(message, "OK, I can help you with that!");
-    bot.startConversation(message, askSubject);
-});
-
 
 controller.hears(['salesforce: create contact', 'salesforce: new contact'], 'message_received', function (bot, message) {
 
